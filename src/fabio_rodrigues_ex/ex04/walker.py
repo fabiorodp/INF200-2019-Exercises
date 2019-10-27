328bde77e96db4db9ae8cfc7410aab81712ab123
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-10-27 23:23:16 +0100

    Methods implemented

--- src/fabio_rodrigues_ex/ex04/walker.py
+++ src/fabio_rodrigues_ex/ex04/walker.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from random import randint
 
 __author__ = 'Fábio Rodrigues Pereira'
 __email__ = 'faro@nmbu.no'
@@ -7,28 +6,34 @@ __email__ = 'faro@nmbu.no'
 
 class Walker:
     def __init__(self, initial, home):
-        self.initial = initial
+        self.x = [initial]
         self.home = home
+        self.steps = 0
 
     def move(self):
         """take one step"""
-        pass
+        self.x.append(self.x[self.steps] + 1)
+        self.steps += 1
 
     def is_at_home(self):
-        """to check whether the student is at home
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
-        """
-        pass
+        """to check whether the student is at home"""
+        if self.x[self.steps] == self.home:
+            return True
+        return False
 
     def get_position(self):
         """to access the students current position"""
-        pass
+        return self.x[self.steps]
 
     def get_steps(self):
         """to access the number of steps the student has taken in
         total"""
+        return self.steps
+
+    def walking_process(self):
         pass
 
-    pass
+
+
+
+
