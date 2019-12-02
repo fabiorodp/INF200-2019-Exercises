Sign of the sigma formula corrected

src/fabio_rodrigues_ex/ex06/logistic_regression.py
@@ -25,7 +25,7 @@ def sigmoid(z):
     sigmoidal_transformed_z : np.ndarray
         Transformed input.
     """
-    return 1 / (1 - np.exp(-z))
+    return 1 / (1 + np.exp(-z))
 
 
 def predict_proba(coef, X):